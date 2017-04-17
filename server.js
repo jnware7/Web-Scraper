620d97f3b77ac01bdb90c613a317e20e18f00a32
rogercamps <rogercamps@me.com>
2017-04-17 15:36:46 -0700

    setup server.js file

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app = express();
+
+app.get('/scrape', function(req, res){
+
+
+})
+
+app.listen('8081')
+console.log('Guess the artist');
+exports = module.exports = app;
