b8a53659ec579bd97b879596b1602c38aece3de0
rogercamps <rogercamps@me.com>
2017-04-18 15:44:33 -0700

    scrape data of single URL

--- server.js
+++ server.js
@@ -4,18 +4,75 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
-app.get('/scrape', function(req, res)
+
+// THE BROAD
+
+app.get('/scrape', function(req, res){
+
   url = 'http://www.thebroad.org/art/jean%E2%80%90michel-basquiat';
 
   request(url, function(error, response, html){
-    if(!error){
-      var $ = cheerio.load(html);
-      var title, image;
-      var json = { title: "", image: ""}
+    if (error){
+      response.status(500).send(error.message)
+      return
     }
+
+
+    var $ = cheerio.load(html);
+    var paintings = []
+
+    $('.view-content > .views-row').each(function(){
+      var painting = $(this)
+      var title = painting.find('a[href]').text()
+      var imageURL = painting.find('img').attr('src')
+      paintings.push({
+        title: title,
+        imageURL: imageURL
+      })
+    })
+    fs.writeFile('output.json', JSON.stringify(paintings, null, 2), function(err){
+      if (err) throw err
+      console.log('File successfully written! - Check your project directory for output.json file');
+    })
+    res.send(paintings)
   })
 })
 
-app.listen('8081')
-console.log('Guess the artist');
-exports = module.exports = app;
+// app.get('/scrape', function(req, res){
+//
+//   url = 'http://www.thebroad.org/art/keith-haring';
+//
+//   request(url, function(error, response, html){
+//     if (error){
+//       response.status(500).send(error.message)
+//       return
+//     }
+//
+//
+//     var $ = cheerio.load(html);
+//     var paintings = []
+//
+//     $('.view-content > .views-row').each(function(){
+//       var painting = $(this)
+//       var title = painting.find('a[href]').text()
+//       var imageURL = painting.find('img').attr('src')
+//       paintings.push({
+//         title: title,
+//         imageURL: imageURL
+//       })
+//     })
+//     fs.writeFile('output.json', JSON.stringify(paintings, null, 2), function(err){
+//       if (err) throw err
+//       console.log('File successfully written! - Check your project directory for output.json file');
+//     })
+//     res.send(paintings)
+//     console.log('party time?', paintings)
+//   })
+// })
+
+
+
+
+app.listen('8081');
+console.log('Guess the artist!');
+module.exports = app;
